3033d22a05665c163b1de3c91319ffeee1d8bc8a
HatsuneMiku <999hatsune@gmail.com>
2013-08-08 09:08:37 +0900

    modify searchlights.d

--- src/searchlights.d
+++ src/searchlights.d
@@ -2,14 +2,13 @@
   searchlights.d
 */
 
-import std.utf;
 import std.windows.charset;
+import std.utf;
 import std.stdio;
 
 int main(string[] args)
 {
-  foreach(int i, string arg; args){
+  foreach(int i, string arg; args)
     writefln("arg[%d] = [%s]", i, arg);
-  }
   return 0;
 }
